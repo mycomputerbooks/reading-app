c91d01c2377d623177bebd5e9ee0e05ba4ed6a65
Mark <mprouty@mycomputerbooks.com>
2018-05-07 22:10:56 -0500

    commit2 through webstorm

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,5 +7,5 @@ import Modal from './modules/Modal';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-//var stickyHeader = new StickyHeader();
+//var stickyHeader = new StickyHeader();  notes
 var modal = new Modal();
